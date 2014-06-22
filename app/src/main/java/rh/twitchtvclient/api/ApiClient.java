ce96859d277abea35f13c094d94d260e0a9ff7f0
ryanh <ryanheitner@gmail.com>
2014-06-22 13:48:44 +0300

    Initial Commit

---
+++ app/src/main/java/rh/twitchtvclient/api/ApiClient.java
@@ -0,0 +1,31 @@
+package rh.twitchtvclient.api;
+
+import rh.twitchtvclient.model.JustinTvStreamData;
+
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.http.GET;
+import retrofit.http.Query;
+
+public class ApiClient {
+    private static TwitchTvApiInterface sTwitchTvService;
+
+    public static TwitchTvApiInterface getTwitchTvApiClient() {
+        if (sTwitchTvService == null) {
+            RestAdapter restAdapter = new RestAdapter.Builder()
+                    .setEndpoint("http://api.justin.tv/api")
+                    .build();
+
+            sTwitchTvService = restAdapter.create(TwitchTvApiInterface.class);
+        }
+
+        return sTwitchTvService;
+    }
+
+    public interface TwitchTvApiInterface {
+        @GET("/stream/list.json")
+        void getStreams(@Query("limit") int limit, @Query("offset") int offset, Callback<List<JustinTvStreamData>> callback);
+    }
+}
